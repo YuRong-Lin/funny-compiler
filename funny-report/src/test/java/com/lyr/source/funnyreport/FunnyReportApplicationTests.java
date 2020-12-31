9aff25b4702efddcd9c28cc854cadd4592790130
林昱榕 <linyurong@jianke.com>
2020-12-31 18:29:16 +0800

    新增模块：报表引擎（待实现）

---
+++ funny-report/src/test/java/com/lyr/source/funnyreport/FunnyReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyr.source.funnyreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

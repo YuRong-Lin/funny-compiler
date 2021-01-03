5bae5302be146e3420121ee26c60971228fbdc4c
YuRong-Lin <linyr0122@gmail.com>
2021-01-03 17:32:36 +0800

    增加模块：front-end-algorithm(前端算法实战)

---
+++ front-end-algorithm/src/test/java/com/lyr/source/frontendalgorithm/FrontEndAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyr.source.frontendalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
